16ca6d9713de876929117b312ff912cfd6fe82cf
zhquyuan <30763649+zhquyuan@users.noreply.github.com>
2019-07-04 15:37:47 +0800

    Add files via upload

--- IAsk/qiuwen.sql
+++ IAsk/qiuwen.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50615
 File Encoding         : 65001
 
-Date: 2019-07-04 10:30:49
+Date: 2019-07-04 15:33:51
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,8 +23,16 @@ CREATE TABLE `qiuwen_quesanswer` (
   `q_id` int(11) NOT NULL,
   `u_id` varchar(255) NOT NULL,
   `a_text` text NOT NULL,
-  `ans_time` datetime NOT NULL
+  `ans_time` datetime NOT NULL,
+  KEY `user_answer` (`u_id`),
+  KEY `ques_answer` (`q_id`),
+  CONSTRAINT `ques_answer` FOREIGN KEY (`q_id`) REFERENCES `qiuwen_question` (`q_id`) ON DELETE CASCADE,
+  CONSTRAINT `user_answer` FOREIGN KEY (`u_id`) REFERENCES `qiuwen_user` (`usr_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of qiuwen_quesanswer
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for qiuwen_question
@@ -39,8 +47,19 @@ CREATE TABLE `qiuwen_question` (
   `is_top` tinyint(1) NOT NULL,
   `sub_time` datetime NOT NULL,
   `category` varchar(255) NOT NULL,
-  PRIMARY KEY (`q_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`q_id`),
+  KEY `user_ques` (`u_id`),
+  CONSTRAINT `user_ques` FOREIGN KEY (`u_id`) REFERENCES `qiuwen_user` (`usr_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of qiuwen_question
+-- ----------------------------
+INSERT INTO `qiuwen_question` VALUES ('1', '1029789014@qq.com', '如何理解梯度和方向导数？', '0', '0', '0', '2019-07-04 11:27:43', 'math');
+INSERT INTO `qiuwen_question` VALUES ('2', '1029789014@qq.com', '如何理解大数定理？', '0', '0', '0', '2019-07-04 11:29:38', 'math');
+INSERT INTO `qiuwen_question` VALUES ('3', '1171078496@qq.com', '如何评价詹姆斯？', '0', '0', '0', '2019-07-04 11:30:30', 'pe');
+INSERT INTO `qiuwen_question` VALUES ('4', '1171078496@qq.com', '如何入门C++', '0', '0', '0', '2019-07-04 11:31:16', 'computer');
+INSERT INTO `qiuwen_question` VALUES ('5', '1171078496@qq.com', '如何入门Python？', '0', '0', '0', '2019-07-04 11:31:56', 'computer');
 
 -- ----------------------------
 -- Table structure for qiuwen_user
@@ -59,13 +78,33 @@ CREATE TABLE `qiuwen_user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+-- Records of qiuwen_user
+-- ----------------------------
+INSERT INTO `qiuwen_user` VALUES ('曲卓涵', '1029789014@qq.com', '123456', '1', '1', '21', '西北工业大学', '软件1班');
+INSERT INTO `qiuwen_user` VALUES ('史嘉辉', '1171078496@qq.com', '123456', '1', '1', '21', '西北工业大学', '404宿舍');
+INSERT INTO `qiuwen_user` VALUES ('吕炳旭', '2461677579@qq.com', '123456', '1', '1', '21', '西北工业大学', '舍长');
+INSERT INTO `qiuwen_user` VALUES ('谢子昆', '287706314@qq.com', '123456', '1', '1', '21', '西北工业大学', '白毛浮绿水，我会打排球');
+INSERT INTO `qiuwen_user` VALUES ('孙启星', '3292160950@qq.com', '123456', '1', '1', '21', '西北工业大学', '软件3班');
+INSERT INTO `qiuwen_user` VALUES ('杨洁', '380279801@qq.com', '123456', '1', '1', '12', '西北工业大学', '这个人很懒，木有介绍');
+INSERT INTO `qiuwen_user` VALUES ('马嘉伟', '764155839@qq.com', '123456', '1', '1', '21', '西北工业大学', '宁夏马子哥');
+INSERT INTO `qiuwen_user` VALUES ('摇摇子', '982069087@qq.com', '123456', '1', '1', '21', '西北工业大学', '416宿舍');
+
+-- ----------------------------
 -- Table structure for qiuwen_userfriend
 -- ----------------------------
 DROP TABLE IF EXISTS `qiuwen_userfriend`;
 CREATE TABLE `qiuwen_userfriend` (
   `u_id` varchar(255) NOT NULL,
-  `friend_id` varchar(255) NOT NULL
+  `friend_id` varchar(255) NOT NULL,
+  KEY `user` (`u_id`),
+  KEY `friend` (`friend_id`),
+  CONSTRAINT `friend` FOREIGN KEY (`friend_id`) REFERENCES `qiuwen_user` (`usr_id`) ON DELETE CASCADE,
+  CONSTRAINT `user` FOREIGN KEY (`u_id`) REFERENCES `qiuwen_user` (`usr_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of qiuwen_userfriend
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for qiuwen_userstar
@@ -73,5 +112,13 @@ CREATE TABLE `qiuwen_userfriend` (
 DROP TABLE IF EXISTS `qiuwen_userstar`;
 CREATE TABLE `qiuwen_userstar` (
   `u_id` varchar(255) NOT NULL,
-  `q_id` int(11) NOT NULL
+  `q_id` int(11) NOT NULL,
+  KEY `uuser` (`u_id`),
+  KEY `qques` (`q_id`),
+  CONSTRAINT `qques` FOREIGN KEY (`q_id`) REFERENCES `qiuwen_question` (`q_id`) ON DELETE CASCADE,
+  CONSTRAINT `uuser` FOREIGN KEY (`u_id`) REFERENCES `qiuwen_user` (`usr_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of qiuwen_userstar
+-- ----------------------------
